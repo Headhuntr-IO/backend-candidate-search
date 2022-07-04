804922c87474d64bc49db3e86e6f6afab7389cd7
LM Bibera <secret.transaction@gmail.com>
2022-07-04 18:26:41 +0800

    add sample usage of environment variables

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Candidate Search (Server)
+
+## Deploying an App
+```shell
+serverless deploy -s dev --param="VAR2=DEV_VAR2"
+```
--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
 import json
-
+import os
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event,
+        "app": os.environ['APP_NAME']
     }
 
     response = {
