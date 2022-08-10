03ba6c992f941026aff8f11700ca89495e4bc2a7
LM Bibera <secret.transaction@gmail.com>
2022-08-10 14:16:11 +0800

    extract env

--- create.py
+++ create.py
@@ -2,22 +2,26 @@ import json
 import os
 import boto3
 
-# db = boto3.client('dynamodb')
+table = os.environ['CANDIDATE_SEARCH_TABLE_NAME']
+db = boto3.client('dynamodb')
 
 
 def handle(event, context):
-
     request_body = __extract_body_as_json(event)
 
     parameters = request_body['parameters']
-    print(parameters)
+    print(table)
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(request_body)
+        "body": json.dumps({
+            "table": table,
+            "parameters": parameters
+        })
     }
 
     return response
+
 
 def __extract_body_as_json(event):
     if event['body']:
